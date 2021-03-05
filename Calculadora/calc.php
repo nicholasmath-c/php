4e8a00db8be1e196a1151d7b7fdf29421487cdc0
Nicholas M. A. Costa <59261158+Mus4ng@users.noreply.github.com>
2021-03-05 03:11:36 -0300

    Update calc.php

--- Calculadora/calc.php
+++ Calculadora/calc.php
@@ -4,12 +4,15 @@
         $n1 = $_POST["n1"];
         $n2 = $_POST["n2"];
         $operacao = $_POST["operacao"];
+        $resultado;
 
         switch($operacao){
-            case "Soma": echo $n1+$n2; break;
-            case "Subtração": echo $n1-$n2; break;
-            case "Multiplicação": echo $n1*$n2; break;
-            case "Divisão": echo $n1/$n2; break;
+            case "Soma": $resultado=$n1+$n2; break;
+            case "Subtração": $resultado=$n1-$n2; break;
+            case "Multiplicação": $resultado=$n1*$n2; break;
+            case "Divisão": $resultado=$n1/$n2; break;
         }
+
+        echo $resultado;
     }
 ?>
